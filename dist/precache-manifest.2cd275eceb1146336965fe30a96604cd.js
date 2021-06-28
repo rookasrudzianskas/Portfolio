ddab03e18dd9e074cb3809efe8fa9ef9809c31af
Rokas <rookas.rudzenskas@gmail.com>
2021-06-28 16:02:40 +0300

    Rokas

---
+++ dist/precache-manifest.2cd275eceb1146336965fe30a96604cd.js
@@ -0,0 +1,318 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee088fd1d0e5adb038f176d6a528709",
+    "url": "/android-chrome-192x192.png"
+  },
+  {
+    "revision": "d32c5d24a868c341c6e34fd491d2a160",
+    "url": "/android-chrome-192x192.png.gz"
+  },
+  {
+    "revision": "1a178d647aa85ca647e3c9a4b5ca2794",
+    "url": "/android-chrome-512x512.png"
+  },
+  {
+    "revision": "887ef4860cd20f32a6470e8bc1fdcf09",
+    "url": "/android-chrome-512x512.png.gz"
+  },
+  {
+    "revision": "aee088fd1d0e5adb038f176d6a528709",
+    "url": "/apple-touch-icon.png"
+  },
+  {
+    "revision": "d32c5d24a868c341c6e34fd491d2a160",
+    "url": "/apple-touch-icon.png.gz"
+  },
+  {
+    "revision": "4bd7fa63d0b2f3364643",
+    "url": "/css/404.1d2c4aa3.css"
+  },
+  {
+    "revision": "5982fedfb40f36c9a8d3e96c3dbb6246",
+    "url": "/css/404.1d2c4aa3.css.gz"
+  },
+  {
+    "revision": "c8439ff98a1de6ff0cd8",
+    "url": "/css/about.2b35f259.css"
+  },
+  {
+    "revision": "2ee14086395d38307463b77cc38e220e",
+    "url": "/css/about.2b35f259.css.gz"
+  },
+  {
+    "revision": "acfe1be1c34728932207",
+    "url": "/css/app.c515e799.css"
+  },
+  {
+    "revision": "0326e33d9e63440420706f8bdf3a2b11",
+    "url": "/css/app.c515e799.css.gz"
+  },
+  {
+    "revision": "086548af3845fba9d205",
+    "url": "/css/work.5187ca25.css"
+  },
+  {
+    "revision": "df0f48e1326dbbb42b182412879602c9",
+    "url": "/css/work.5187ca25.css.gz"
+  },
+  {
+    "revision": "b93ec51258c42db65afae5992100f9ef",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "5f1749727d0dea58e4a9a96fb9452d3e",
+    "url": "/favicon-16x16.png.gz"
+  },
+  {
+    "revision": "aa000215f14392366f52f263d9a31b15",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "35a065e7d4d2aaaf38e0d835c366afe8",
+    "url": "/favicon-32x32.png.gz"
+  },
+  {
+    "revision": "fefb0b94b2bd6d31d29a53f3eeff9d74",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "5ad96f810c43c876a2b1689fc783564a",
+    "url": "/favicon.ico.gz"
+  },
+  {
+    "revision": "788464ec6bac1ce024e3d543f6fbbd6c",
+    "url": "/fonts/smw.788464ec.woff"
+  },
+  {
+    "revision": "f21027fb68d2f6254f2b07420613f775",
+    "url": "/fonts/smw.f21027fb.woff2"
+  },
+  {
+    "revision": "9b6f11fca47aaca3e01d3b0b4e65e78d",
+    "url": "/img/am-pa.9b6f11fc.png"
+  },
+  {
+    "revision": "55940065b748a16b4cd03b2c672ad161",
+    "url": "/img/bg-mario.55940065.png"
+  },
+  {
+    "revision": "e1d03181411325b9ad7a1b569ffbed26",
+    "url": "/img/bg-mario.55940065.png.gz"
+  },
+  {
+    "revision": "07bdc7140ac4cd854c25d4e40a0cfcaa",
+    "url": "/img/body.07bdc714.png"
+  },
+  {
+    "revision": "c419e2bff9820f2f346d6a8798d91e5c",
+    "url": "/img/bonfim.c419e2bf.png"
+  },
+  {
+    "revision": "f0b84e57aefd32a69899e07ead0cfb17",
+    "url": "/img/clothes.f0b84e57.png"
+  },
+  {
+    "revision": "49f17c074e8263136b64fc6b9a00a846",
+    "url": "/img/cloud1.49f17c07.png"
+  },
+  {
+    "revision": "4c98c0ae4d0807b537f90b687f5d7e2d",
+    "url": "/img/cloud2.4c98c0ae.png"
+  },
+  {
+    "revision": "d92e0705b00d9852a736ba08dc6b436b",
+    "url": "/img/clouds-1.d92e0705.png"
+  },
+  {
+    "revision": "5e79ba0ff5d0b8e5e32dc87286a64c83",
+    "url": "/img/clouds-2.5e79ba0f.png"
+  },
+  {
+    "revision": "e34bd674d4ddbe6cb9b4b8c992c434de",
+    "url": "/img/clouds-3.e34bd674.png"
+  },
+  {
+    "revision": "65c42c3a163927676b4f6e07eead489e",
+    "url": "/img/fan-1.65c42c3a.png"
+  },
+  {
+    "revision": "49636bfd8dcf34715f6b7816c0f68494",
+    "url": "/img/fan-1.65c42c3a.png.gz"
+  },
+  {
+    "revision": "126ebaa431983131be6c55d70ad60579",
+    "url": "/img/fan-2.126ebaa4.png"
+  },
+  {
+    "revision": "9363e49fb673dd24616e3f5b0c4ae12f",
+    "url": "/img/fan-2.126ebaa4.png.gz"
+  },
+  {
+    "revision": "ffd8a04beb347f3a4c84e3b28342d953",
+    "url": "/img/goya.ffd8a04b.png"
+  },
+  {
+    "revision": "8e8c79d2d211611242059c41bd1a4bf7",
+    "url": "/img/grass1.8e8c79d2.png"
+  },
+  {
+    "revision": "829ae6a24014702918863c8945db24c3",
+    "url": "/img/grass2.829ae6a2.png"
+  },
+  {
+    "revision": "3e614a0a270339a7302349b9b0142adb",
+    "url": "/img/head.3e614a0a.png"
+  },
+  {
+    "revision": "dea0c7713c9b25aa89c6ba54a3b73b58",
+    "url": "/img/keyboard.dea0c771.png"
+  },
+  {
+    "revision": "f1f0faa8169934e03a49556aff008924",
+    "url": "/img/metals.f1f0faa8.png"
+  },
+  {
+    "revision": "1f44f74483eb4eba838d498dcc8d2fb6",
+    "url": "/img/minas-church.1f44f744.png"
+  },
+  {
+    "revision": "79b41ee7a8f52995fd6819b96f213e6e",
+    "url": "/img/ocean-base.79b41ee7.jpg"
+  },
+  {
+    "revision": "83dde13c4e7baa53b8a67ab9cf65a9d0",
+    "url": "/img/ocean-waves.83dde13c.png"
+  },
+  {
+    "revision": "b5548c37f84b9415b26dd65b942a755a",
+    "url": "/img/pieces.b5548c37.png"
+  },
+  {
+    "revision": "b2450ad075d4be02a5a3ed6491509237",
+    "url": "/img/rio-waterfall.b2450ad0.png"
+  },
+  {
+    "revision": "8e5c1de2e5271bed2e220db2d2c13d49",
+    "url": "/img/rio.8e5c1de2.png"
+  },
+  {
+    "revision": "0fd1ff3ab86052d9fddef287bab0f0e2",
+    "url": "/img/rokas.0fd1ff3a.png"
+  },
+  {
+    "revision": "88ce0c3e9e1a9a8144e4714f7b2e270e",
+    "url": "/img/rokas.0fd1ff3a.png.gz"
+  },
+  {
+    "revision": "41fad4c68afdf8870eb1b79d84bfb237",
+    "url": "/img/salvador-church.41fad4c6.png"
+  },
+  {
+    "revision": "b362590f0670516f525187b0448c4a04",
+    "url": "/img/sky-mask.b362590f.png"
+  },
+  {
+    "revision": "7b029e055d22e34808306a3e00ed8e36",
+    "url": "/img/sky.7b029e05.jpg"
+  },
+  {
+    "revision": "2340b7d1fc8f6ea13f7a418353f069c5",
+    "url": "/img/sky.7b029e05.jpg.gz"
+  },
+  {
+    "revision": "65adf4e2edca86b9a2112eb09b358c9b",
+    "url": "/img/wing.65adf4e2.png"
+  },
+  {
+    "revision": "1d266cebfc7725059ee250f15e3cbe94",
+    "url": "/img/wing.65adf4e2.png.gz"
+  },
+  {
+    "revision": "8a7deec9508d4d2bdb27cd4ed01e85f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6518d583dc5ee9b756fd258fb4d08a02",
+    "url": "/index.html.gz"
+  },
+  {
+    "revision": "4bd7fa63d0b2f3364643",
+    "url": "/js/404.2ce700a4.js"
+  },
+  {
+    "revision": "1bf2fec6f96950b00f61275271e16c2c",
+    "url": "/js/404.2ce700a4.js.gz"
+  },
+  {
+    "revision": "c8439ff98a1de6ff0cd8",
+    "url": "/js/about.56cb0c60.js"
+  },
+  {
+    "revision": "84f571df84081dac3628b4caee7f9764",
+    "url": "/js/about.56cb0c60.js.gz"
+  },
+  {
+    "revision": "acfe1be1c34728932207",
+    "url": "/js/app.0e56b1c4.js"
+  },
+  {
+    "revision": "45353be23c79c9eb90df43253545e15d",
+    "url": "/js/app.0e56b1c4.js.gz"
+  },
+  {
+    "revision": "ed885c74b1e4b784a2e4",
+    "url": "/js/chunk-vendors.ff992ebf.js"
+  },
+  {
+    "revision": "bc442992d621a7af54e3fbdd596ef278",
+    "url": "/js/chunk-vendors.ff992ebf.js.gz"
+  },
+  {
+    "revision": "086548af3845fba9d205",
+    "url": "/js/work.1946b026.js"
+  },
+  {
+    "revision": "100366d484107ba29fcb7d45c8db6514",
+    "url": "/js/work.1946b026.js.gz"
+  },
+  {
+    "revision": "e90d4353f6fead1ec4e7efa28cb08656",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "96681f57288569cae9e1656d70a88317",
+    "url": "/manifest.json.gz"
+  },
+  {
+    "revision": "a8b8504b29a5186cd3f16f4d99ca787e",
+    "url": "/media/smw_keyhole_exit.a8b8504b.ogg"
+  },
+  {
+    "revision": "01d1241b245bb4b740dbba2bf4677146",
+    "url": "/media/smw_power-up.01d1241b.ogg"
+  },
+  {
+    "revision": "1f4184e68f4b57e84f1f0bbb916156a5",
+    "url": "/media/smw_power-up_appears.1f4184e6.ogg"
+  },
+  {
+    "revision": "6db08fe8499f5d6c12e6831a52d44c6b",
+    "url": "/media/smw_princess_help.6db08fe8.ogg"
+  },
+  {
+    "revision": "cc3fd5510a4058e0e1ac5fe4e5e23b1e",
+    "url": "/media/smw_stomp.cc3fd551.ogg"
+  },
+  {
+    "revision": "f0ed2462e1f2074ca93f671a3cea580d",
+    "url": "/media/smw_stomp_no_damage.f0ed2462.ogg"
+  },
+  {
+    "revision": "5c0ca65eb64184424d47e73a46f33f8b",
+    "url": "/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "433843081254d8c120f773c69853c715",
+    "url": "/safari-pinned-tab.svg.gz"
+  }
+]);
